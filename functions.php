751e6b4a7433d31d29db6863f906865954d86eb8
Jeremy Leggat <jleggat@asu.edu>
2023-03-09 17:46:34 -0700

    Add missing bootstrap_navigation to functions.php

--- functions.php
+++ functions.php
@@ -261,6 +261,49 @@ function hook_parselyTrack()
 }
 add_action('wp_footer', 'hook_parselyTrack');
 
+// Navigation Menu Adjustments
+
+    /* Add class to navigation sub-menu */
+class bootstrap_navigation extends Walker_Nav_Menu
+{
+
+    function start_lvl(&$output, $depth = 0, $args = array())
+    {
+        $output .= "\n<ul class=\"dropdown-menu\">\n";
+    }
+
+    function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
+    {
+        $item_html = '';
+        parent::start_el($item_html, $item, $depth, $args);
+
+        if ($item->is_dropdown && $depth === 0 ) {
+            $item_html = str_replace('<a', '<a class="dropdown-toggle" data-toggle="dropdown"', $item_html);
+            $item_html = str_replace('</a>', ' <b class="caret"></b></a>', $item_html);
+        }
+        $output .= $item_html;
+    }
+
+    function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output)
+    {
+        if ($element->current ) {
+            $element->classes[] = 'active';
+        }
+            $element->is_dropdown = !empty($children_elements[$element->ID]);
+
+        if ($element->is_dropdown ) {
+            if ($depth === 0 ) {
+                $element->classes[] = 'dropdown';
+            } elseif ($depth === 1 ) {
+                // Extra level of dropdown menu,
+                // as seen in http://twitter.github.com/bootstrap/components.html#dropdowns
+                $element->classes[] = 'dropdown-submenu';
+            }
+        }
+            parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+    }
+}
+     
 /* Display Pages In Navigation Menu */
 if (!function_exists('bootstrap_menu')) {
     function bootstrap_menu()
