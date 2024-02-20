fff9eb3a656292e380db054f0900dae6df8d056e
Adnan Alam <adnan@loladigitalmedia.com>
2024-02-20 11:52:50 -0700

    Yoast SEO social card updates.

--- functions.php
+++ functions.php
@@ -914,7 +914,7 @@ function get_story_byline() {
 function update_wpseo_meta_author_filter( $author_name, $presentation ){
   $author_name = get_story_byline();
 
-  add_filter( 'wpseo_enhanced_slack_data', function($data, $author_name) {
+  add_filter( 'wpseo_enhanced_slack_data', function($data) {
       $array = ["By" => $author_name];
       return $array;
   });
