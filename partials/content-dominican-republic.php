0dc264bd665f5b70459321a2227743c64090772b
Adnan Alam <adnan@loladigitalmedia.com>
2024-03-19 09:27:34 -0700

    Borderlands Dominican Republic landing page

--- partials/content-dominican-republic.php
+++ partials/content-dominican-republic.php
@@ -39,8 +39,7 @@
                     <img src="<?php echo $photo; ?>" alt="" title="" />
                   <?php } ?>
                   <h3><a href="<?php echo get_permalink($story->ID); ?>"><?php echo get_the_title($story->ID); ?></a></h3>
-                  <p><?php echo get_field('story_tease', $story->ID); ?></p>
-                  <a href="<?php echo get_permalink($story->ID); ?>" class="button">Read More <i class="fa-regular fa-angle-right"></i></a>
+                  <p><?php echo get_field('story_tease', $story->ID); ?></p>                  
                 </div>
               <?php
             }
