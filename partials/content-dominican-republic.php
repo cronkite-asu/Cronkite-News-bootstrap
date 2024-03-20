f6ee3255d630fe39c233284cdc04dcb9f49355ca
Adnan Alam <adnan@loladigitalmedia.com>
2024-03-20 11:43:34 -0700

    Borderlands Dominican Republic landing page

--- partials/content-dominican-republic.php
+++ partials/content-dominican-republic.php
@@ -134,8 +134,8 @@
 
 <script>
     // star wars
-    TweenMax.set(".haiti-dr .scene2 .intro-text", {autoAlpha:0});
-    //TweenMax.set(".star-wars .scene3 .crawl", {autoAlpha:0});
+    TweenMax.set(".haiti-dr .scene2", {autoAlpha:0});
+    TweenMax.set(".haiti-dr .scroll-down", {autoAlpha:0});
 
     ScrollTrigger.create({
       trigger: ".haiti-dr",
@@ -153,6 +153,7 @@
       },
       opacity: 0,
       onComplete: function() {
+          TweenMax.set(".haiti-dr .scene1", {autoAlpha:0});
           TweenMax.set(".haiti-dr .scene2", {autoAlpha:1});
           TweenMax.set(".haiti-dr .scroll-down", {autoAlpha:0});
 
