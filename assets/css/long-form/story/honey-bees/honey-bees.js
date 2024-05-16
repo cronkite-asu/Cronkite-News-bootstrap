e293767babaf9cfce70a72aadf09f65aeb81e17d
Adnan Alam <adnan@loladigitalmedia.com>
2024-05-16 13:06:47 -0700

    Honey bee javascript.

--- assets/css/long-form/story/honey-bees/honey-bees.js
+++ assets/css/long-form/story/honey-bees/honey-bees.js
@@ -4,20 +4,11 @@ $(document).foundation(),$(document).ready(function(){
   // adding scroll event
   window.addEventListener('scroll', function(){
     // detects new state and compares it with the new one
-    if ((document.body.getBoundingClientRect()).top > scrollPos) {
-  		//document.getElementById('info-box').setAttribute('data-scroll-direction', 'UP');
-      console.log('UP');
-      //document.getElementsByTagName("body")[0].style.cursor = "url('https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-up-cursor-icon.png'), auto";
-      //$("html, body").css("cursor: url('https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-up-cursor-icon.png'), auto !important");
+    if ((document.body.getBoundingClientRect()).top > scrollPos) {  		
       $("html, body").css({'cursor': 'url(https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-up-cursor-icon.png), default'});
   	} else {
-      console.log('DOWN');
-      //$("html, body").css("cursor: url('https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-down-cursor-icon.png'), auto !important");
       $("html, body").css({'cursor': 'url(https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-down-cursor-icon.png), default'});
-  		//document.getElementById('info-box').setAttribute('data-scroll-direction', 'DOWN');
-      //document.getElementsByTagName("body")[0].style.cursor = "url('https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-down-cursor-icon.png'), auto";
     }
-    console.log(document.getElementsByTagName("body")[0].style.cursor);
   	// saves the new position for iteration.
   	scrollPos = (document.body.getBoundingClientRect()).top;
   });
