8e0d9b8513edec2a1c2a0bf140c1686998c25e01
Adnan Alam <adnan@loladigitalmedia.com>
2024-05-16 11:46:23 -0700

    Honey bee css.

--- assets/css/long-form/story/honey-bees/honey-bees.js
+++ assets/css/long-form/story/honey-bees/honey-bees.js
@@ -7,11 +7,11 @@ $(document).foundation(),$(document).ready(function(){
     if ((document.body.getBoundingClientRect()).top > scrollPos) {
   		//document.getElementById('info-box').setAttribute('data-scroll-direction', 'UP');
       console.log('UP');
-      document.body.style.cursor = "cursor: url('https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-up-cursor-icon.png'), auto;";
+      document.body.style.cursor = "url('https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-up-cursor-icon.png'), auto";
   	} else {
       console.log('DOWN');
   		//document.getElementById('info-box').setAttribute('data-scroll-direction', 'DOWN');
-      document.body.style.cursor = "cursor: url('https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-down-cursor-icon.png'), auto;";
+      document.body.style.cursor = "url('https://cronkitenews.azpbs.org/wp-content/uploads/2024/05/bee-down-cursor-icon.png'), auto";
     }
   	// saves the new position for iteration.
   	scrollPos = (document.body.getBoundingClientRect()).top;
