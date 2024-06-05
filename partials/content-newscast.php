23a9a4ae3f8707394aaba32172b727aff49b8476
Adnan Alam <adnan@loladigitalmedia.com>
2024-06-05 11:26:37 -0700

    Newscast page update with youtube playlist embed.

--- partials/content-newscast.php
+++ partials/content-newscast.php
@@ -17,64 +17,11 @@
         <?php
           $category_id = get_queried_object_id();
         ?>
-        <div id="latest-stories" class="large-8 medium-12 small-12 cell story-content">
-          <?php
-            $paged = get_query_var('paged');
-        $arg = [
-            'post_type'        => 'post',
-            'order'            => 'DESC',
-            'orderby'        => 'date',
-            'category_name' =>  'newscast',
-        ];
-        $newscast = new WP_Query($arg);
+        <div id="latest-stories" class="large-12 medium-12 small-12 cell story-content">
 
-        if ($newscast->have_posts()) {
-            while ($newscast->have_posts()) {
-                $newscast->the_post();
-                ?>
-                <div class="grid-x grid-margin-x story-results-stack">
-                  <div class="large-8 medium-8 small-8 cell">
-                    <h4><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h4>
-                    <p><?php echo get_field('story_tease'); ?></p>
-                    <div class="pub_date">
-                      <time datetime="<?php echo ap_date(); ?>"><?php echo ap_date(); ?></time>
-                    </div>
-                  </div>
-                  <div class="large-4 medium-4 small-4 cell">
-                    <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail(get_the_ID()); ?></a>
-                  </div>
-                  <div class="large-12 medium-12 small-12 cell">
-                    <hr />
-                  </div>
-                </div>
+          <iframe width="100%" height="560" src="https://www.youtube.com/embed/videoseries?si=cwpUkoFJB01a3Bys&amp;list=PLQ10JyjBe_u5ltL9YqidL1JttoQrbORBg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <?php wp_reset_query(); ?>
 
-                    <?php
-            }
-        } else {
-            ?>
-
-                <?php
-        }
-        ?>
-            <div class="grid-x grid-margin-x story-results-stack">
-              <div class="large-12 medium-12 small-12 cell">
-                <?php bootstrap_pagination(); ?>
-              </div>
-            </div>
-          <?php
-        wp_reset_query();
-        ?>
-
-        </div>
-
-        <div class="large-4 medium-12 small-12 cell sidebar">
-            <?php dynamic_sidebar('Sidebar Author - 2020'); ?>
-
-            <?php if ($category_id == 7022) { ?>
-            <div class="large-4 medium-12 small-12 cell sidebar">
-                <?php dynamic_sidebar('Health Insider'); ?>
-            </div>
-            <?php } ?>
         </div>
       </div>
     </div>
