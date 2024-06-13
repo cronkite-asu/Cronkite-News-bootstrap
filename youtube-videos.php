22c15e68400c9c3be95fdc47119ecb481a799b82
Adnan Alam <adnan@loladigitalmedia.com>
2024-06-13 14:50:46 -0700

    Youtube video list test.

--- youtube-videos.php
+++ youtube-videos.php
@@ -10,48 +10,33 @@ $apiKey = 'AIzaSyD18QZUABnzCt1YPyxgVZbgVZpjQ1PKRGI';
 $channelId = 'UCO8tHWm0LQy3QWFcnZeV4CQ';
 $resultsNumber = '50000';
 
-$requestUrl = 'https://www.googleapis.com/youtube/v3/search?key=' . $apiKey . '&channelId=' . $channelId . '&part=snippet,id&maxResults=' . $resultsNumber .'&order=date';
-// Try file_get_contents first
-if( function_exists( file_get_contents ) ) {
-    $response = file_get_contents( $requestUrl );
-    $json_response = json_decode( $response, TRUE );
+// API config
+$API_Key    = 'AIzaSyD18QZUABnzCt1YPyxgVZbgVZpjQ1PKRGI';
+$Channel_ID = 'UCO8tHWm0LQy3QWFcnZeV4CQ';
+$Max_Results = 5000;
 
-} else {
-    // No file_get_contents? Use cURL then...
-    if( function_exists( 'curl_version' ) ) {
-        $curl = curl_init();
-        curl_setopt( $curl, CURLOPT_URL, $requestUrl );
-        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, TRUE );
-        $response = curl_exec( $curl );
-        curl_close( $curl );
-        $json_response = json_decode( $response, TRUE );
-
-    } else {
-        // Unable to get response if both file_get_contents and cURL fail
-        $json_response = NULL;
-    }
+// Get videos from channel by YouTube Data API
+$apiData = @file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$Channel_ID.'&maxResults='.$Max_Results.'&key='.$API_Key.'');
+if($apiData){
+    echo 'HERE';
+    $videoList = json_decode($apiData);
+}else{
+    echo 'Invalid API key or channel ID.';
 }
-
-// If there's a JSON response
-if( $json_response ) {
-    $i = 1;
-    echo '<div class="youtube-channel-videos">';
-    foreach( $json_response['items'] as $item ) {
-        $videoTitle = $item['snippet']['title'];
-        $videoID = $item['id']['videoId'];
-        //$videoThumbnail = $item['snippet']['thumbnails']['high']['url'];
-
-        if( $videoTitle && $videoID ) {
-            echo '<div class="youtube-channel-video-embed vid-' . $videoID . ' video-' . $i++ . '"><iframe width="500" height="300" src="https://www.youtube.com/embed/' . $videoID . '" frameborder="0" allowfullscreen>' . $videoTitle . '</iframe></div>';
+if(!empty($videoList->items)){
+    echo 'HERE 2';
+    foreach($videoList->items as $item){
+        // Embed video
+        if(isset($item->id->videoId)){
+            echo '
+            <div class="yvideo-box">
+                <iframe width="280" height="150" src="https://www.youtube.com/embed/'.$item->id->videoId.'" frameborder="0" allowfullscreen></iframe>
+                <h4>'. $item->snippet->title .'</h4>
+            </div>';
         }
     }
-    echo '</div><!-- .youtube-channel-videos -->';
-
-// If there's no response
-} else {
-    // Display error message
-    echo '<div class="youtube-channel-videos error"><p>No videos are available at this time from the channel specified!</p></div>';
-    }
+}else{
+    echo '<p class="error">'.$apiError.'</p>';
+}
 
 ?>
