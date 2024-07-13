37cac95323100cb277179d093da6767fc9680eb6
Adnan Alam <adnan@loladigitalmedia.com>
2024-07-13 10:47:18 -0700

    Olympics and health page updates.

--- partials/content-olympics-2024.php
+++ partials/content-olympics-2024.php
@@ -90,6 +90,7 @@
                         $photoURL = get_the_post_thumbnail_url($latestNews->ID);
                         $photoImg = get_the_post_thumbnail($latestNews->ID);
 
+                        echo $latestNews->ID;
                         // save main story ID
                         $repeatStoriesArray[] = $latestNews->ID;
 
