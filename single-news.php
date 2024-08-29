4f3158d996a61247ee1ef79a7686a372c91ac5a2
Adnan Alam <adnan@loladigitalmedia.com>
2024-08-29 10:30:05 -0700

    Testing social share buttons.

--- single-news.php
+++ single-news.php
@@ -672,7 +672,7 @@
                <div class="report-typo"><a class="hollow button" href="#">Report a Typo</a></div>
              </div>
 
-             <div class="a2a_kit a2a_default_style">
+             <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
                <a class="a2a_button_facebook"><i class="fa-brands fa-x-twitter"></i></a>
                <a class="a2a_button_linkedin"><i class="fa-brands fa-linkedin"></i></a>
                <a class="a2a_button_facebook"><i class="fa-brands fa-square-facebook"></i></a>
