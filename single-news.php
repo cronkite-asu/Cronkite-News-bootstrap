66b021995f3bdca59b39d7a3b30422e2e6cbed9b
Adnan Alam <adnan@loladigitalmedia.com>
2024-08-29 11:05:58 -0700

    Finalized social share button on posts.

--- single-news.php
+++ single-news.php
@@ -302,9 +302,10 @@
               <div class="a2a_kit a2a_default_style">
                 <a class="a2a_button_facebook"><i class="fa-brands fa-square-facebook"></i></a>
                 <a class="a2a_button_linkedin"><i class="fa-brands fa-linkedin"></i></a>
+                <a class="a2a_button_reddit"><i class="fa-brands fa-square-reddit"></i></a>
                 <a class="a2a_button_twitter"><i class="fa-brands fa-x-twitter"></i></a>
                 <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
-              </div>               
+              </div>
              </div>
            </div>
          </div>
