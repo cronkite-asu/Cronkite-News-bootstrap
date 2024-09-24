e0c348c9c9b1cb5680b4f4bbc2c2f890d8d6ce00
Adnan Alam <adnan@loladigitalmedia.com>
2024-09-24 14:12:42 -0700

    Related story section code.

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -651,14 +651,16 @@ add_shortcode('video-embed-right', 'video_embed_right');
 function related_box_grid_list($atts, $content = null) {
     if (current_user_can('administrator')) {
       if (isset($atts['block-name'])) {
-        $postID = get_the_ID();
+
+        $post_categories = wp_get_post_categories( get_the_ID() );
+        $cats = array();
+        print_r($cats);
         $result = '<div class="related-story-block">';
         if ($postID == 237109 || $postID == 236532) {
           $result .= '<div class="banner">Related story</div>';
         } else {
           $result .= '<h4>Related story</h4>';
-        }
-        $result .= 'ID: '.$postID;
+        }        
         $result .= '<ul>';
         $result .= '<li><a href="" target="_blank"><div class="img">IMG</div><h4>HEADLINE</h4></a></li>';
         $result .= '<li><a href="" target="_blank"><div class="img">IMG</div><h4>HEADLINE</h4></a></li>';
