6a8cb29831292b0608200c9b395713a05b08ce68
Adnan Alam <adnan@loladigitalmedia.com>
2024-10-03 11:28:51 -0700

    Election 2024 template.

--- partials/content-election2024.php
+++ partials/content-election2024.php
@@ -78,14 +78,14 @@ foreach ($mainStoryList as $mainStory) {
         <?php
             $mainStoryList = get_field('stories', 237021);
             $mainStoryCounter = 0;
-            foreach ($mainStoryList as $mainStory) {
-              if ($mainStoryCounter > 2) {
         ?>
               <div class="large-3 medium-3 small-12 cell story">
                 <div class="grid-x grid-padding-x">
                   <div class="large-12 medium-12 small-12 cell story">
                     <ul>
         <?php
+            foreach ($mainStoryList as $mainStory) {
+              if ($mainStoryCounter > 2) {
                 $permalink = get_permalink($mainStory);
                 $summary = get_field('story_tease', $mainStory);
                 if (get_field('use_short_headline', $mainStory) == 'yes' && get_field('homepage_headline', $mainStory) != '') {
@@ -98,15 +98,15 @@ foreach ($mainStoryList as $mainStory) {
                 $topStoriesArray[] = $mainStory;
         ?>
                     <li><a href="<?php echo $permalink; ?>" target="_blank"><?php echo $title; ?></a></li>
-        <?php } ?>
-                    </ul>
-                  </div>
-                </div>
-              </div>
         <?php
+              }
               $mainStoryCounter++;
             }
         ?>
+                  </ul>
+                </div>
+              </div>
+            </div>
       </div>
     </div>
   </div>
