3172e1991e739051caaf9aabfde0123a1f2781da
Adnan Alam <adnan@loladigitalmedia.com>
2024-10-22 14:17:24 -0700

    Election page css

--- partials/content-election2024.php
+++ partials/content-election2024.php
@@ -215,39 +215,7 @@ foreach ($mainStoryList as $mainStory) {
             // save main story ID
             $topStoriesArray[] = $profile;
         ?>
-            <div class="large-6 medium-6 small-12 cell story">
-              <a href="<?php echo $permalink; ?>" target="_blank"><?php echo get_the_post_thumbnail($profile); ?><h3 style="margin-top:15px;"><?php echo $title; ?></h3></a>
-            </div>
-        <?php } ?>
-        <?php $candidateCounter++; ?>
-        <?php } ?>
-            <hr>
-          </div>
-        </div>
-
-        <!-- district 1 -->
-        <div class="large-6 medium-6 small-12 cell candidate-divider">
-          <div class="grid-x grid-padding-x">
-            <div class="large-12 medium-12 small-12 cell candidate-position">
-              <p>District 1</p>
-            </div>
-        <?php
-        // left side stories
-        $candidateProfilesList = get_field('candidate_profiles', 237021);
-        $candidateCounter = 0;
-        foreach ($candidateProfilesList as $profile) {
-            if ($candidateCounter >= 4 && $candidateCounter < 5) {
-            $permalink = get_permalink($profile);
-            $summary = get_field('story_tease', $profile);
-            if (get_field('use_short_headline', $profile) == 'yes' && get_field('homepage_headline', $profile) != '') {
-                $title = get_field('homepage_headline', $profile);
-            } else {
-                $title = get_the_title($profile);
-            }
-            // save main story ID
-            $topStoriesArray[] = $profile;
-        ?>
-            <div class="large-6 medium-6 small-12 cell story district1">
+            <div class="large-6 medium-6 small-6 cell story">
               <a href="<?php echo $permalink; ?>" target="_blank"><?php echo get_the_post_thumbnail($profile); ?><h3 style="margin-top:15px;"><?php echo $title; ?></h3></a>
             </div>
         <?php } ?>
@@ -258,7 +226,7 @@ foreach ($mainStoryList as $mainStory) {
         </div>
 
         <!-- district 3 -->
-        <div class="large-6 medium-6 small-12 cell">
+        <div class="large-6 medium-6 small-12 cell candidate-divider">
           <div class="grid-x grid-padding-x">
             <div class="large-12 medium-12 small-12 cell candidate-position">
               <p>District 3</p>
@@ -289,7 +257,7 @@ foreach ($mainStoryList as $mainStory) {
         </div>
 
         <!-- district 5 -->
-        <div class="large-6 medium-6 small-12 cell candidate-divider">
+        <div class="large-6 medium-6 small-12 cell">
           <div class="grid-x grid-padding-x">
             <div class="large-12 medium-12 small-12 cell candidate-position">
               <p>District 5</p>
@@ -319,7 +287,7 @@ foreach ($mainStoryList as $mainStory) {
         </div>
 
         <!-- district 7 -->
-        <div class="large-6 medium-6 small-12 cell">
+        <div class="large-6 medium-6 small-12 cell candidate-divider">
           <div class="grid-x grid-padding-x">
             <div class="large-12 medium-12 small-12 cell candidate-position">
               <p>District 7</p>
@@ -345,6 +313,38 @@ foreach ($mainStoryList as $mainStory) {
         <?php } ?>
         <?php $candidateCounter++; ?>
         <?php } ?>
+          </div>
+        </div>
+
+        <!-- district 1 -->
+        <div class="large-6 medium-6 small-12 cell">
+          <div class="grid-x grid-padding-x">
+            <div class="large-12 medium-12 small-12 cell candidate-position">
+              <p>District 1</p>
+            </div>
+        <?php
+        // left side stories
+        $candidateProfilesList = get_field('candidate_profiles', 237021);
+        $candidateCounter = 0;
+        foreach ($candidateProfilesList as $profile) {
+            if ($candidateCounter >= 4 && $candidateCounter < 5) {
+            $permalink = get_permalink($profile);
+            $summary = get_field('story_tease', $profile);
+            if (get_field('use_short_headline', $profile) == 'yes' && get_field('homepage_headline', $profile) != '') {
+                $title = get_field('homepage_headline', $profile);
+            } else {
+                $title = get_the_title($profile);
+            }
+            // save main story ID
+            $topStoriesArray[] = $profile;
+        ?>
+            <div class="large-6 medium-6 small-12 cell story district1">
+              <a href="<?php echo $permalink; ?>" target="_blank"><?php echo get_the_post_thumbnail($profile); ?><h3 style="margin-top:15px;"><?php echo $title; ?></h3></a>
+            </div>
+        <?php } ?>
+        <?php $candidateCounter++; ?>
+        <?php } ?>
+            <hr>
           </div>
         </div>
 
