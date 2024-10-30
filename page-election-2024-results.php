002f61673ace0eb67759e718b4ef982522ac3918
Adnan Alam <adnan@loladigitalmedia.com>
2024-10-30 09:46:14 -0700

    Election results page.

---
+++ page-election-2024-results.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Template Name: Election 2024 results page
+ */
+
+get_header('election2024'); ?>
+
+  <?php get_template_part('partials/content', 'election2024-results'); ?>
+
+<?php get_footer('election2022'); ?>
---
+++ partials/content-election2024-results.php
@@ -0,0 +1,8 @@
+<!-- main body container -->
+<div id="election-container" class="grid-container">
+  <div class="grid-x grid-padding-x align-stretch">
+    <div class="large-12 medium-12 small-12 cell">
+      <?php echo get_the_content(); ?>
+    </div>
+  </div>
+</div>
