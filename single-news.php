ac81c8da006de50692c5909ea5a45e39c033c5e2
Adnan Alam <adnan@loladigitalmedia.com>
2024-10-30 14:35:07 -0700

    Fix youtube embed issue.

--- single-news.php
+++ single-news.php
@@ -186,7 +186,7 @@
                      the_row();
                      $staffID = get_sub_field('cn_staff');
                      $cnStaffCount = count((array)$staffID);
-                     print_r($staffID);
+                     
                      if ($staffID != '') {
                        foreach ($staffID as $key => $val) {
                            $args = [
