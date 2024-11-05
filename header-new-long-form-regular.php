2e66a5252c67fd8a7468b9a9ee891b6d115e140a
Adnan Alam <adnan@loladigitalmedia.com>
2024-11-05 07:44:16 -0700

    Election layout.

--- header-new-long-form-regular.php
+++ header-new-long-form-regular.php
@@ -182,6 +182,7 @@ if (isset($settings['text-color']) && $settings['text-color'] != '') {
           <!-- logo -->
           <div class="logo">
             <a href="<?php echo home_url(); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/cronkite-news.svg" alt="Cronkite News" title="Cronkite News" /></a>
+            <a class="election-2024-logo" href="https://cronkitenews.azpbs.org/election-2024"><img src="https://cronkitenews.azpbs.org/wp-content/uploads/2024/09/cn-election-2024.svg" alt="Election 2024" title="Election 2024" /></a>            
           </div>
           <!-- main navigation -->
           <div class="nav_links">
