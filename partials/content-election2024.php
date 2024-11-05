206ed45030218bc3eaa043b943bce92a7df375d6
Adnan Alam <adnan@loladigitalmedia.com>
2024-11-05 07:08:24 -0700

    Election layout.

--- partials/content-election2024.php
+++ partials/content-election2024.php
@@ -60,7 +60,11 @@ $candidateProfiles = [];
                 <div class="large-6 medium-6 small-12 cell main-story">
                   <div class="grid-x grid-padding-x">
                     <div class="large-12 medium-12 small-12 cell">
-                      <a href="<?php echo $permalink; ?>"><?php echo get_the_post_thumbnail($mainStory); ?><div class="circle pulse red"></div><h3><?php echo $title; ?></h3></a>
+                      <a href="<?php echo $permalink; ?>"><?php echo get_the_post_thumbnail($mainStory); ?>
+                        <div class="headline">
+                          <div class="circle pulse red"></div><h3><?php echo $title; ?></h3>
+                        </div>
+                      </a>
                     </div>
                   </div>
                 </div>
