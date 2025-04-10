97239d13009525b5aca021ac4682ce5bf55f3917
Adnan Alam <adnan@loladigitalmedia.com>
2025-04-10 10:29:19 -0700

    Social picks template.

---
+++ page-social-picks.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Template Name: Social Picks
+ */
+
+get_header('new2019'); ?>
+
+  <?php get_template_part('partials/content', 'social-picks'); ?>
+
+<?php get_footer('new2020'); ?>
---
+++ partials/content-social-picks.php
@@ -0,0 +1,31 @@
+<!-- header -->
+<div class="page-header">
+  <div class="grid-container main-stories">
+    <div class="grid-x grid-padding-x single-story-block">
+      <div class="large-12 medium-12 small-12 cell">
+        <h1>Social Stories</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="grid-container content fluid">
+  <?php
+    $args = array(
+      'post_type' => 'post',
+      'post_status' => 'publish',
+      'posts_per_page' => -1,
+      'category__in' => array( 37726 )
+    );
+    $query = new WP_Query($args);
+  ?>
+
+  <div class="grid-x grid-margin-x">
+    <?php if ($query->have_posts() ) : ?>
+      <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+        <div class="cell large-4 small-4"><a href="<?php echo get_permalink(); ?>" target="_blank"><?php echo get_the_post_thumbnail(get_the_ID()); ?></a></div>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </div>
+
+</div>
